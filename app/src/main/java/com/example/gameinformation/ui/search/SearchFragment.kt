521827e95b8473eaa43d60aaf0a2c4372abeb5f2
Yunus Erbek <yunus_erbek@windowslive.com>
2023-03-23 13:26:04 +0300

    fixed

---
+++ app/src/main/java/com/example/gameinformation/ui/search/SearchFragment.kt
@@ -0,0 +1,77 @@
+package com.example.gameinformation.ui.search
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import com.example.gameinformation.R
+import androidx.appcompat.widget.SearchView
+import com.example.gameinformation.common.delegation.viewBinding
+import com.example.gameinformation.databinding.FragmentSearchBinding
+import com.example.gameinformation.ui.adapter.HomeAdapter
+import com.example.gameinformation.ui.adapter.LastSearchedAdapter
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class SearchFragment : Fragment(R.layout.fragment_search) {
+    private val gamesAdapter by lazy { HomeAdapter() }
+
+    private val binding by viewBinding(FragmentSearchBinding::bind)
+    private val viewModel by viewModels<SearchVM>()
+    private val lastAdapter by lazy { LastSearchedAdapter() }
+
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        searchQueryListen()
+        getLastData()
+        lastWordClick()
+    }
+     private fun searchQueryListen() {
+
+         binding.svSearchGame.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+             override fun onQueryTextSubmit(query: String): Boolean {
+                 searchGames(query)
+                 return false
+             }
+
+             override fun onQueryTextChange(newText: String): Boolean {
+                 return false
+             }
+         })
+     }
+   fun getLastData() = viewLifecycleOwner.lifecycleScope.launch {
+
+       viewModel.lastSearchedWordsState.collect {
+           lastAdapter.lastList(it)
+           binding.lastRv.adapter = lastAdapter
+       }
+   }
+    private fun searchGames(query: String) {
+        //viewModel.searchGame(query)
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.lifecycleScope.launch {
+                viewModel.searchGame.collect { state ->
+                    state?.let {
+                        binding.searchRv.adapter =gamesAdapter
+                        gamesAdapter.submitData(state)
+
+                    }
+                }
+            }
+        }
+
+    }
+    fun lastWordClick(){
+     lastAdapter.onWordClick ={
+         searchGames(it)
+
+     }
+ }
+
+}
---
+++ app/src/main/java/com/example/gameinformation/ui/search/SearchVM.kt
@@ -0,0 +1,44 @@
+package com.example.gameinformation.ui.search
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import com.example.gameinformation.features.home.domain.entity.GamesUi
+import com.example.gameinformation.features.home.domain.usecase.LastSearchedWordsUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+@HiltViewModel
+class SearchVM @Inject constructor(
+
+    private val lastSearchedWordsUseCase: LastSearchedWordsUseCase,
+
+) : ViewModel() {
+    private var _searchGame = MutableStateFlow<PagingData<GamesUi>>(PagingData.empty())
+    var searchGame = _searchGame.asStateFlow()
+
+    private var _lastSearchedWords= MutableStateFlow<List<String>> (emptyList())
+    var lastSearchedWordsState = _lastSearchedWords.asStateFlow()
+
+    init {
+        //astSearchedWords()
+
+
+    }
+
+//    fun lastSearchedWords() = viewModelScope.launch {
+//        lastSearchedWordsUseCase().collect { result ->
+//            _lastSearchedWords.value = result
+//
+//        }
+//    }
+ // fun searchGame(query:String) = viewModelScope.launch {
+ //     searchUseCase(20,query).cachedIn(viewModelScope).collect { result ->
+ //         _searchGame.value = result
+ //     }
+
+ // }
+}
